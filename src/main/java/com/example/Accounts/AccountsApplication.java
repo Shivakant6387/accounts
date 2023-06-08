d278c79820f45f8c4a413c3e202f405a93ed7f9e
Shivakant6387 <shivakant5875@gmail.com>
2023-06-08 23:48:01 +0530

    docker file

--- src/main/java/com/example/Accounts/AccountsApplication.java
+++ src/main/java/com/example/Accounts/AccountsApplication.java
@@ -1,8 +1,13 @@
 package com.example.Accounts;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaClient
